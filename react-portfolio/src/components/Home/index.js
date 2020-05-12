8a51d87334be949ec4095633719f4c11efe8da51
KantKontrol <nick.derissio.tech@gmail.com>
2020-05-12 14:27:25 -0400

    adjusted container and image

--- react-portfolio/src/components/Home/index.js
+++ react-portfolio/src/components/Home/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ContentArea from "../ContentArea";
 import HeroImage from "../HeroImage";
+import BorderedContainer from "../BorderedContainer";
+import BorderedImage from "../BorderedImage";
 
 
 function Home() {
@@ -8,10 +10,17 @@ function Home() {
         <ContentArea>
             <div className="row">
                 <div className="col-12">
-                    <HeroImage image="./space.jpg" randomQuote={true} animate={true}/>
+                    <HeroImage image="./images/space.jpg" randomQuote={true} animate={true}/>
                 </div>
             </div>
 
+            <div className="row">
+                <BorderedContainer>
+                        <BorderedImage src="./images/mich.jpg" alt="picture of me"></BorderedImage>
+                    
+
+                </BorderedContainer>
+            </div>
         </ContentArea>
     );
 }
