c4573afa22d5ca19fcb07854ee376e5df03ac465
KantKontrol <nick.derissio.tech@gmail.com>
2020-06-15 13:05:14 -0400

    changed waypoint image

--- react-portfolio/src/components/Portfolio/index.js
+++ react-portfolio/src/components/Portfolio/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ContentArea from "../ContentArea";
 import HeroImage from "../HeroImage";
 import ProjectJumbotron from "../ProjectJumbotron";
-import Fade from "react-reveal/Fade"
+import Slide from "react-reveal/Slide"
 
 
 function Portfolio() {
@@ -67,11 +67,11 @@ function Portfolio() {
             <HeroImage image="./images/space5.jpg" randomQuote={true} animate={false} quotes={["Projects"]}/>
             <ContentArea>
                 <div className="row" style={{margin: 10 + "px"}}>
-                    <Fade bottom>
+                    <Slide left>
                         {projects.map(e => {
                             return <ProjectJumbotron key={e.title} src={e.src} title={e.title} techUsed={e.techUsed} desc={e.desc} github={e.github} deployed={e.deployed}/>
                         })}
-                    </Fade>
+                    </Slide>
                 </div>
             </ContentArea>
         </div>
