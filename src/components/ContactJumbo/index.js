7c050ec7e99254b8ddf0b8b3d6ef32d6f7881c1d
KantKontrol <nick.derissio.tech@gmail.com>
2020-08-17 16:51:33 -0400

    moved resume to bottom of contact jumbo

--- src/components/ContactJumbo/index.js
+++ src/components/ContactJumbo/index.js
@@ -14,8 +14,8 @@ export default function ContactJumbo(props){
             <hr className="my-4" />
                     <ContactDisplay src="fab fa-github" href="https://github.com/KantKontrol" text="Github" dblock={true}/>
                     <ContactDisplay src="fab fa-linkedin" href="https://www.linkedin.com/in/nicholas-derissio" text="LinkedIn" dblock={true} />
-                    <ContactDisplay src="far fa-file-alt" href="/images/port/resume.pdf" text="Resume" dblock={true}/>
                     <ContactDisplay src="fab fa-instagram" href="https://www.instagram.com/nicholas.uber.dev/" text="Instagram" dblock={true} />
+                    <ContactDisplay src="far fa-file-alt" href="/images/port/resume.pdf" text="Resume" dblock={true}/>
         </div>
     );
 }
